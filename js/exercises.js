ce57405fcfc8cde056eaefb910b27d0cc15b101d
Kassiyet <kaszi.exe@gmail,com>
2024-11-03 13:09:30 +0500

    dropdown filter

--- js/exercises.js
+++ js/exercises.js
@@ -1,57 +1,110 @@
-const url = 'https://exercisedb.p.rapidapi.com/exercises?limit=20&offset=0';
-const options = {
-	method: 'GET',
-	headers: {
-		'x-rapidapi-key': 'abe1f9a9f9mshc8cc1d5f2b456dfp111760jsnf4125069d072',
-		'x-rapidapi-host': 'exercisedb.p.rapidapi.com'
-	}
-};
-const fetchData = async () => {
-    try {
-        const response = await fetch(url, options);
+// const url = 'https://exercisedb.p.rapidapi.com/exercises?limit=10&offset=0';
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'x-rapidapi-key': 'abe1f9a9f9mshc8cc1d5f2b456dfp111760jsnf4125069d072',
+// 		'x-rapidapi-host': 'exercisedb.p.rapidapi.com'
+// 	}
+// };
+// const fetchData = async () => {
+//     try {
+//         const response = await fetch(url, options);
         
-        if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
-        const jsonString = await response.text();
-        const data = JSON.parse(jsonString); 
-        return data; // Access your data as an object
-    } catch (error) {
-        console.error("Error:", error);
-    }
-}
+//         if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+//         const jsonString = await response.text();
+//         const data = JSON.parse(jsonString); 
+//         return data; // Access your data as an object
+//     } catch (error) {
+//         console.error("Error:", error);
+//     }
+// }
+const exercises = [
+    {
+        bodyPart: "waist",
+        equipment: "body weight",
+        gifUrl: "https://v2.exercisedb.io/image/PVjcbbo4xrl3Xf",
+        id: "0001",
+        name: "3/4 sit-up",
+        secondaryMuscles: ['hip flexors', 'lower back'],
+        target: "abs"
+    },
+    {
+        bodyPart: "waist",
+        equipment: "body weight",
+        gifUrl: "https://v2.exercisedb.io/image/PVjcbbo4xrl3Xf",
+        id: "0002",
+        name: "sit-up",
+        secondaryMuscles: ['hip flexors', 'lower back'],
+        target: "abs"
+    },
+    {
+        bodyPart: "upper legs",
+        equipment: "body weight",
+        gifUrl: "https://v2.exercisedb.io/image/PVjcbbo4xrl3Xf",
+        id: "0003",
+        name: "legs ",
+        secondaryMuscles: ['hip flexors', 'lower back'],
+        target: "legs"
+    }   
+];
+
+
+// Filter exercises with dropdown lsit
+const bodyPartBtns = document.querySelectorAll(".dropdown-item");
+bodyPartBtns.forEach(btn => {
+    btn.addEventListener("click", event => {
+        const expectedBodyPart = event.target.innerText;
+        console.log(expectedBodyPart);
+        const filteredExercises = exercises.filter(exercise => exercise.bodyPart === expectedBodyPart);
+        renderExercise(filteredExercises);
+    });
+});
+
+// Filter exercises with search input
 
 const renderExercise = exercises =>{
+    // Import container to display the exercise cards
+    const container = document.getElementById("exercise-container");
+    container.innerHTML = "";
+    if(exercises.length === 0){
+        container.innerHTML = "<h4>No exercises found!</h4>";
+        return;
+    }
     for(exercise of exercises){
-        const contianer = document.getElementById("exercise-container");
-        contianer.innerHTML = "";
 
+        // Exercise box (card) container
         const exerciseCard = document.createElement("div");
-        exerciseCard.id = "exercise-box";
-        exerciseCard.className = "bg-secondary text-center p-3 border";
+        exerciseCard.id = exercise.id;
+        exerciseCard.className = "exercise-box rounded text-center p-3 border col-sm-8 col-md-5 col-lg-3 col-xl-3 col-xxl-2";
 
+        // Image (gif) for exercise
         const img = document.createElement("img");
         img.src = exercise.gifUrl;
         img.className = "rounded mb-4";
         img.style.width = "90%";
         exerciseCard.appendChild(img);
 
+        // Exercise name
         const nameField = document.createElement("h4");
         nameField.innerText = exercise.name;
         exerciseCard.appendChild(nameField);
 
+        // Body part display
         const bodyPartField = document.createElement("p");
-        bodyPartField.innerText = `Body Part: ${exercise.bodyPart}`;
+        bodyPartField.innerHTML = `Body Part: <strong>${exercise.bodyPart}</strong>`;
         exerciseCard.appendChild(bodyPartField);
 
+        // Target area display
         const targetAreaField = document.createElement("p");
-        targetAreaField.innerText = `Target Area: ${exercise.target}`;
+        targetAreaField.innerHTML = `Target Area:  <strong>${exercise.target}</strong>`;
         exerciseCard.appendChild(targetAreaField);
 
-
-        contianer.appendChild(exerciseCard);
-
-        //img.src = exercise.gifUrl;
+        // Add exercise card to container
+        container.appendChild(exerciseCard);
         console.log(exercise);
     }
 }
-fetchData()
-.then(renderExercise);
+//initial render to display all exercises
+renderExercise(exercises);
+// fetchData()
+// .then(renderExercise);
